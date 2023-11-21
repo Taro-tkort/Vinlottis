4e47310e2c5d3af8f8e7c81b6ca630ad27030296
Joel Mörlin <jml@datarespons.no>
2023-11-21 10:40:44 +0100

    fiksa det men ikke egentlig

--- sheet_manip.py
+++ sheet_manip.py
@@ -20,24 +20,31 @@ path = "Book.xlsx"
 workbook = load_workbook(filename=path)
 sheet = workbook.active
 
-data = [[],[],[]] #list for the extracted data
+sheet['D4'] = "test"
+
+data = [[],[],[],[]] #list for the extracted data
 #extract relevat data from sheet
 for row in sheet.iter_rows(min_col=1, max_col=2, values_only=True):
     data[0].append(row[0]) #name
-    data[1].append(row[1]) #tickets
+    data[1].append(row[1]) #money 
     print(row)
+
+#convert money to tickets
+for val in data[1]:
+    tmp = val/20 #convert to tickets
+    data[2].append(tmp) #add to data 3 thingy
 
 #calculate total points
 ratio = 200
 sum = 0
-for val in data[1]:
+for val in data[2]:
     sum += val
 
 #calculate ratios and place inb slot 3
 checksum = 0 #sum to check the averages
-for val in data[1]:
+for val in data[2]:
     tmp = yeild(val)
-    data[2].append(int(round(tmp)))
+    data[3].append(int(round(tmp)))
     checksum += int(round(tmp))
     
 if int(checksum) == int(ratio) :
@@ -46,12 +53,24 @@ else :
     print("Shits Wrong!! is not 200 but is %d" % checksum)
 print(data)
 
-#write the ranges to the excel document
+#write the tickets to the excel doc
 iterator = 1 #iterator running through the for loop
 rowstr = "C" #the row the range is to be set at 
 for val in data[2]:
     cordtmp = rowstr + str(iterator)
     print("Writing %d to %s" % (val, cordtmp))
-    sheet['C1']=val
+    sheet[cordtmp]=val
     iterator += 1
+
+#write the ranges to the excel document
+iterator = 1 #iterator running through the for loop
+rowstr = "D" #the row the range is to be set at 
+for val in data[3]:
+    cordtmp = rowstr + str(iterator)
+    print("Writing %d to %s" % (val, cordtmp))
+    sheet[cordtmp]=val
+    iterator += 1
+print("Saving as new excel sheet")
+workbook.save("liste.xlsx")
 print("Finished! sould be written to excel now :)")
+
